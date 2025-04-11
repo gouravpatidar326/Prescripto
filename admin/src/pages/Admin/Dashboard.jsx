1f4cb86ca287ac1f847a67a6b084b4425aae63e5
gouravpatidar326 <156186747+gouravpatidar326@users.noreply.github.com>
2025-04-12 01:46:34 +0530

    Update Dashboard.jsx

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -71,29 +71,6 @@ const Dashboard = () => {
               <p className="text-center text-gray-500 py-4">No recent bookings available</p>
             )
           }
-
-
-          {/* {
-            dashData.latestAppointments.map((item, index) => (
-              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
-                <img className='rounded-full w-10' src={item.docData.image} alt="" />
-
-                <div className='flex-1 text-sm'>
-                  <p className='text-gray-800 font-medium'>{item.docData.name}</p>
-                  <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
-                </div>
-
-                {
-                  item.cancelled
-                    ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
-                    : item.isCompleted
-                      ? <p className='text-green-500 text-xs font-medium'>Completed</p>
-                      : <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
-                }
-
-              </div>
-            ))
-          } */}
         </div>
 
       </div>
